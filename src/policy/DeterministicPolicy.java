49fed1527ee5450c6d177c42e1cf3ab32deeadf2
Marciano <Marciano@MSI.wireless.unimaas.local>
2017-03-23 14:43:44 +0100

    Commit error cause I don't give a shit

--- src/policy/DeterministicPolicy.java
+++ src/policy/DeterministicPolicy.java
@@ -1,5 +1,6 @@
 package policy;
 
+import java.awt.Point;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,16 +8,26 @@ import main.Action;
 import main.State;
 
 public class DeterministicPolicy extends HashMap<State, Action> implements Policy {
-	/** generated id for serialization */
-	private static final long serialVersionUID = 291698286088712191L;
 
-	public DeterministicPolicy(Map<State, Action> policy) {
-		super(policy);
-	}
+    /**
+     * generated id for serialization
+     */
+    private static final long serialVersionUID = 291698286088712191L;
 
-	@Override
-	public Action getAction(State state) {
-		return super.get(state);
-	}
+    public DeterministicPolicy(Map<State, Action> policy) {
+        super(policy);
+    }
+
+    @Override
+    public Action getAction(State state) {
+        Point P1 = state.getP1();
+        Point P2 = state.getP2();
+        if(state.getPossession() == State.FIRST_PLAYER){
+            
+        } else {
+            if(P1.x == )
+        }
+        return super.get(state);
+    }
 
 }
