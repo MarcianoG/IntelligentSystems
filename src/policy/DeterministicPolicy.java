9f8794015d7ff1e3847d467d3156db0c25909ecc
Imray <Imray@192.168.0.194>
2017-03-28 13:58:05 +0200

    Determinisitc

--- src/policy/DeterministicPolicy.java
+++ src/policy/DeterministicPolicy.java
@@ -24,14 +24,69 @@ public class DeterministicPolicy extends HashMap<State, Action> implements Polic
 
     @Override
     public Action getAction(State state) {
-        Point P1 = state.getP1();
-        Point P2 = state.getP2();
-        if (state.getPossession() == State.FIRST_PLAYER) {
+        Point p1 = state.getP1();
+        Point p2 = state.getP2();
 
+        //If player is one has the ball
+        //We are always player two
+        if (state.getPossession() == State.SECOND_PLAYER) {
+            /*Has the ball*/
+            //Next to the end
+            if (p1.x == State.MAX_X) {
+                //Next to the goal
+                if (p1.y == 1 || p1.y == 2) {
+                    return Action.EAST;
+                } else if (isBelow(p1, p2) && p1.y == 0) {
+                    return Action.STAND;
+                } else if (isAbove(p1, p2) && p1.y == State.MAX_Y) {
+                   
+                        return Action.EAST;
+                } else if (p1.y == State.MAX_Y) {
+                    return Action.NORTH;
+                } else {
+                    return Action.SOUTH;
+                }
+            } else if (isLeft(p1, p2)) {
+                    return Action.STAND;
+            } else {
+                return Action.EAST;
+            }
+
+        } else if (p1.x == State.MIN_X) {
+            if (p1.y == 1) {
+              
+                    return Action.SOUTH;
+              
+            } else if (p1.y == 2) {
+              
+                    return Action.NORTH;
+               
+            } else if (p1.y == 0) {
+                return Action.SOUTH;
+            } else {
+                return Action.NORTH;
+            }
+        } else if (isLeft(p1, p2)) {
+            return Action.STAND;
         } else {
-
+            return Action.EAST;
         }
-        return super.get(state);
+    }
+
+    private boolean isAbove(Point p1, Point p2) {
+        return p2.y == p1.y + 1;
+    }
+
+    private boolean isLeft(Point p1, Point p2) {
+        return p2.x == p1.x - 1;
+    }
+
+    private boolean isRight(Point p1, Point p2) {
+        return p2.x == p1.x + 1;
+    }
+
+    private boolean isBelow(Point p1, Point p2) {
+        return p2.y == p1.y - 1;
     }
 
 }
