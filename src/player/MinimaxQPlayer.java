b55fb49439b257164be212aabb5b4eabfc029b92
Imray <Imray@192.168.0.194>
2017-03-28 15:43:52 +0200

    MinMax?

--- src/player/MinimaxQPlayer.java
+++ src/player/MinimaxQPlayer.java
@@ -3,6 +3,10 @@ package player;
 import java.util.Map;
 
 import exploration.ExplorationStrategy;
+import java.awt.Point;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Random;
 import main.Action;
 import main.State;
 import main.Triple;
@@ -16,69 +20,168 @@ import scpsolver.lpsolver.SolverFactory;
 import scpsolver.problems.LinearProgram;
 
 public class MinimaxQPlayer implements Player {
-	private boolean player;
-	private State s;
 
-	private Map<Triple<State, Action, Action>, Double> qValues;
-	private Map<State, double[]> pi;
+    private boolean player;
+    private State s;
+    private Point[] currentState;
+    private Action currentAction;
+    private double alpha;
+    private double discountFactor;
+    private double decay;
+    private Map<Triple<State, Action, Action>, Double> qValues = new HashMap();
+    private Map<Point[], Double> values = new HashMap();
+    //private Map<Point[], double[]> pi;
+    //private List<Point[]> allStates = new ArrayList();                      
+    HashMap<Point[], HashMap<Action, Double>> pi = new HashMap(); //PI
 
-	public MinimaxQPlayer(boolean player, double discountFactor, double decay, ExplorationStrategy es) {
-		this.player = player;
-	}
+    public MinimaxQPlayer(boolean player, double discountFactor, double decay, ExplorationStrategy es) {
+        this.player = player;
+        this.discountFactor = discountFactor;
+        this.decay = decay;//All the possible states
+        for (int x1 = 0; x1 < State.MAX_X; x1++) {
+            for (int y1 = 0; y1 < State.MAX_Y; y1++) {
+                for (int x2 = 0; x2 < State.MAX_X; x2++) {
+                    for (int y2 = 0; y2 < State.MAX_Y; y2++) {
+                        if (x1 == x2 && y2 == y1) {
+                            continue;
+                        }
+                        Point[] state = new Point[]{new Point(x1, y1), new Point(x2, y2)};
 
-	@Override
-	public Action chooseAction(State state) {
-		return new RandomPlayer(this.player).chooseAction(state);
-	}
+                        values.put(state, 1d);
+                        HashMap<Action, Double> actions = new HashMap();
+                        for (Action a : Action.values()) {
+                            actions.put(a, 1d / 5.0);
+                            for (Action aa : Action.values()) {
+                                qValues.put(new Triple(state, a, aa), 1d);
+                            }
+                        }
+                        pi.put(state, actions);
 
-	@Override
-	public void receiveReward(double reward, State newState, Action opponentAction) {
+                    }
+                }
+            }
 
-	}
+            alpha = 1d;
+        }
+    }
 
-	public void linearProgramming() {
-		Action[] allActions = Action.values();
+    private static class Pair<A, B> {
 
-		// maximize slack variable (index 0), which represents the value of the
-		// inner minimization
-		double[] maxObjective = new double[allActions.length + 1];
-		maxObjective[0] = 1.0;
-		LinearProgram lp = new LinearProgram(maxObjective);
+        public A a;
+        public B b;
 
-		// all probabilities must add up to 1
-		double[] sum1 = new double[allActions.length + 1];
-		for (int i = 1; i < sum1.length; i++) {
-			sum1[i] = 1.0;
-		}
-		lp.addConstraint(new LinearEqualsConstraint(sum1, 1.0, "a"));
+        public Pair(A a, B b) {
+            this.a = a;
+            this.b = b;
+        }
+    }
 
-		for (int i = 0; i < allActions.length; i++) {
-			// all probabilities must be positive
-			double[] arr1 = new double[maxObjective.length];
-			arr1[i + 1] = 1.0;
-			lp.addConstraint(new LinearBiggerThanEqualsConstraint(arr1, 0, "b" + (i + 1)));
+    Random r = new Random();
 
-			// v <= sum( p(s, action_i) * Q(s,action_i, action_opponent) )
-			// v - sum( p(s, action_i) * Q(s,action_i, action_opponent) ) <= 0
-			double[] arr2 = new double[maxObjective.length];
-			arr2[0] = 1.0;
-			for (int j = 0; j < allActions.length; j++) {
-				arr2[i + 1] = -this.qValues
-						.get(new Triple<State, Action, Action>(this.s, allActions[i], allActions[j]));
-			}
-			lp.addConstraint(new LinearSmallerThanEqualsConstraint(arr2, 0, "c" + (i + 1)));
-		}
-		lp.setMinProblem(false);
+    @Override
+    public Action chooseAction(State state) {
+        s= state;
+        currentState = new Point[]{state.getP1(), state.getP2()};
+        if (r.nextDouble() > 0.99) {
+            currentAction = new RandomPlayer(this.player).chooseAction(state);
+            return currentAction;
+        }
 
-		LinearProgramSolver solver = SolverFactory.newDefault();
-		double[] sol = solver.solve(lp);
-		double[] newPi = new double[allActions.length];
-		System.arraycopy(sol, 1, newPi, 0, newPi.length);
-		this.pi.put(this.s, newPi);
-	}
+        HashMap<Action, Double> prob = pi.get(currentState);
+        double rand = r.nextDouble();
+        double t = 0d;
+        for (Entry<Action, Double> entry : prob.entrySet()) {
+            if (rand < t + entry.getValue()) {
+                currentAction = entry.getKey();
+                return currentAction;
+            }
+            t += entry.getValue();
+        }
+        //if we get here, not good
+        currentAction = new RandomPlayer(this.player).chooseAction(state);
+        return currentAction;
+    }
 
-	public Policy getPolicy() {
-		return new ProbabilisticPolicy(this.pi);
-	}
+    @Override
+    public void receiveReward(double reward, State newState, Action opponentAction) {
+        //Q[s,a,o] := (1-alpha) * Q[s,a,o] + alpha * (rew + gamma * V[s’])
+        Point[] s2 = new Point[]{newState.getP1(), newState.getP2()};
+
+        double currentQ = qValues.get(new Triple(currentState, currentAction, opponentAction));
+        double newQ = 1d - alpha * currentQ + alpha * (reward + discountFactor + values.get(s2));
+        qValues.put(new Triple(currentState, currentAction, opponentAction), newQ);
+        learn(newState,opponentAction);
+    }
+
+    public void learn(State newState, Action opponentAction) {
+        Point[] s2 = new Point[]{newState.getP1(), newState.getP2()};
+        linearProgramming();
+        double min = Double.MIN_VALUE;//Fix me pplease
+        for (Action o : Action.values()) {
+            double sum = 0;
+            //a'
+            for (Action a : Action.values()) {
+                sum += pi.get(s2).get(a) * qValues.get(new Triple(s2, a, o));
+            }
+            if (min > sum) {
+                min = sum;
+            }
+        }
+
+        values.put(s2, min);
+        alpha *= decay;
+    }
+
+    public void linearProgramming() {
+        Action[] allActions = Action.values();
+
+        // maximize slack variable (index 0), which represents the value of the
+        // inner minimization
+        double[] maxObjective = new double[allActions.length + 1];
+        maxObjective[0] = 1.0;
+        LinearProgram lp = new LinearProgram(maxObjective);
+
+        // all probabilities must add up to 1
+        double[] sum1 = new double[allActions.length + 1];
+        for (int i = 1; i < sum1.length; i++) {
+            sum1[i] = 1.0;
+        }
+        lp.addConstraint(new LinearEqualsConstraint(sum1, 1.0, "a"));
+
+        for (int i = 0; i < allActions.length; i++) {
+            // all probabilities must be positive
+            double[] arr1 = new double[maxObjective.length];
+            arr1[i + 1] = 1.0;
+            lp.addConstraint(new LinearBiggerThanEqualsConstraint(arr1, 0, "b" + (i + 1)));
+
+            // v <= sum( p(s, action_i) * Q(s,action_i, action_opponent) )
+            // v - sum( p(s, action_i) * Q(s,action_i, action_opponent) ) <= 0
+            double[] arr2 = new double[maxObjective.length];
+            arr2[0] = 1.0;
+            for (int j = 0; j < allActions.length; j++) {
+                arr2[i + 1] = -this.qValues
+                        .get(new Triple<State, Action, Action>(s, allActions[i], allActions[j]));
+            }
+            lp.addConstraint(new LinearSmallerThanEqualsConstraint(arr2, 0, "c" + (i + 1)));
+        }
+        lp.setMinProblem(false);
+
+        LinearProgramSolver solver = SolverFactory.newDefault();
+        double[] sol = solver.solve(lp);
+        double[] newPi = new double[allActions.length];
+      
+      	HashMap<Action,Double> actions = pi.get(s);
+        int i=0;
+        for(Action key : actions.keySet()){
+          actions.put(key,newPi[i++]);
+        }
+        pi.put(currentState,actions);
+//         System.arraycopy(sol, 1, newPi, 0, newPi.length);
+//         this.pi.put(currentState,(allActions ,newPi));
+    }
+
+    public Policy getPolicy() {
+        return new ProbabilisticPolicy(this.pi);
+    }
 
 }
--- src/policy/ProbabilisticPolicy.java
+++ src/policy/ProbabilisticPolicy.java
@@ -1,34 +1,41 @@
 package policy;
 
+import java.awt.Point;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 import main.Action;
 import main.State;
+import player.RandomPlayer;
 
 public class ProbabilisticPolicy extends HashMap<State, double[]> implements Policy {
-	/** generated id */
-	private static final long serialVersionUID = -7901905496277036579L;
 
-	public ProbabilisticPolicy(Map<State, double[]> policy) {
-		super(policy);
-	}
+    /**
+     * generated id
+     */
+    private static final long serialVersionUID = -7901905496277036579L;
+    private HashMap<Point[], HashMap<Action, Double>> policy;
 
-	@Override
-	public Action getAction(State state) {
-		Action[] allActions = Action.values();
-		double[] prob = super.get(state);
+    public ProbabilisticPolicy(HashMap<Point[], HashMap<Action, Double>> policy) {
+        this.policy = policy;
+    }
 
-		double sum = 0.0;
-		double r = Math.random();
-
-		for (int i = 0; i < prob.length; i++) {
-			sum = sum + prob[i];
-			if (r <= sum) {
-				return allActions[i];
-			}
-		}
-		return allActions[allActions.length - 1];
-	}
+    @Override
+    public Action getAction(State state) {
+        Random r = new Random();
+        Point[] currentState = new Point[]{state.getP1(), state.getP2()};
+        HashMap<Action, Double> prob = policy.get(currentState);
+        double rand = r.nextDouble();
+        double t = 0d;
+        for (Entry<Action, Double> entry : prob.entrySet()) {
+            if (rand < t + entry.getValue()) {
+                return entry.getKey();
+            }
+            t += entry.getValue();
+        }
+        //if we get here, not good
+        return new RandomPlayer(true).chooseAction(state);
+    }
 
 }
